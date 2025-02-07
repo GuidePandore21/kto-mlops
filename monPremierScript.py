5ce5cb4ccf30eb9a553b179744fc31371d2ed0ba
Thomas Lyautey <97942763+GuidePandore21@users.noreply.github.com>
2025-02-07 13:43:58 +0000

    Refactor : réécriture en clean code du fichier monPremierScript.py

--- monPremierScript.py
+++ monPremierScript.py
@@ -3,20 +3,18 @@ import unittest
 """
 Count names with more than seven letters
 """
-def names(prenoms):
-    more_than_seven = 0
-    for prenom in prenoms:
-        if len(prenom) > 7:
-            more_than_seven += 1
-            print(prenom + " est un prénom avec un nombre de lettres supérieur à 7")
-        else:
-            print(prenom + " est un prénom avec un nombre de lettres inférieur ou égal à 7")
-    return more_than_seven
+def compte_les_mots_depassant_strictement_le_nombre_de_lettre_vise(liste_prenoms: list , nombre_lettre: int) -> int:
+    compteur_mots = 0
+    for prenom in liste_prenoms:
+        if len(prenom) > nombre_lettre:
+            compteur_mots += 1
+    return compteur_mots
 
 class TestNamesMethod(unittest.TestCase):
      def test_names(self):
-        prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
-        more_than_seven = names(prenoms=prenoms)
+        liste_prenoms = ["Guillaume", "Gilles", "Juliette", "Antoine", "François", "Cassandre"]
+        nombre_lettre = 7
+        more_than_seven = compte_les_mots_depassant_strictement_le_nombre_de_lettre_vise(liste_prenoms=liste_prenoms, nombre_lettre=nombre_lettre)
         self.assertEqual(more_than_seven, 4)
 
 if __name__ == '__main__':
